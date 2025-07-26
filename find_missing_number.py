d47ad3c1d5e5cd364aa5c419b3c32947fed2ed99
Vinay Mingari <mingarivinay@gmail.com>
2025-07-26 10:24:20 +0530

    Update find_missing_number.py

--- find_missing_number.py
+++ find_missing_number.py
@@ -4,7 +4,6 @@ def find_missing(nums):
     actual_sum = sum(nums)
     return expected_sum - actual_sum
 
-
 print(find_missing([1, 2, 3, 5]))        
 print(find_missing([6, 1, 2, 3, 4]))     
 print(find_missing([2]))    
