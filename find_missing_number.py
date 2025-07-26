44e69206ac9db2b737e5ea66094e3a118251fcb0
Vinay Mingari <mingarivinay@gmail.com>
2025-07-26 10:13:04 +0530

    Added combine_elements

---
+++ find_missing_number.py
@@ -0,0 +1,12 @@
+def find_missing(nums):
+    n = len(nums) + 1  
+    expected_sum = n * (n + 1) // 2  
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
+
+
+print(find_missing([1, 2, 3, 5]))        
+print(find_missing([6, 1, 2, 3, 4]))     
+print(find_missing([2]))    
+print(find_missing(list(range(1, 101))[:-1]))
+
